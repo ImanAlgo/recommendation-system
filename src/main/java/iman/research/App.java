f784ee0aecfacec6ae8c22a96775d1c7ecbebbd0
Iman <iman.mohammadi@drivenow.com.au>
2018-11-20 16:22:55 +1100

    Adding librec

--- src/main/java/iman/research/App.java
+++ src/main/java/iman/research/App.java
@@ -1,13 +1,59 @@
 package iman.research;
 
+import java.util.List;
+
+import net.librec.common.LibrecException;
+import net.librec.conf.Configuration;
+import net.librec.conf.Configuration.Resource;
+import net.librec.data.DataModel;
+import net.librec.data.model.TextDataModel;
+import net.librec.eval.RecommenderEvaluator;
+import net.librec.eval.rating.MAEEvaluator;
+import net.librec.filter.GenericRecommendedFilter;
+import net.librec.filter.RecommendedFilter;
+import net.librec.recommender.Recommender;
+import net.librec.recommender.RecommenderContext;
+import net.librec.recommender.cf.UserKNNRecommender;
+import net.librec.similarity.PCCSimilarity;
+import net.librec.similarity.RecommenderSimilarity;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws LibrecException
     {
         System.out.println( "Hello World!" );
+
+  	// recommender configuration
+	Configuration conf = new Configuration();
+	Resource resource = new Resource("rec/cf/userknn-test.properties");
+	conf.addResource(resource);
+
+	// build data model
+	DataModel dataModel = new TextDataModel(conf);
+	dataModel.buildDataModel();
+	
+	// set recommendation context
+	RecommenderContext context = new RecommenderContext(conf, dataModel);
+	RecommenderSimilarity similarity = new PCCSimilarity();
+	similarity.buildSimilarityMatrix(dataModel);
+	context.setSimilarity(similarity);
+
+	// training
+	Recommender recommender = new UserKNNRecommender();
+	recommender.recommend(context);
+
+	// evaluation
+	RecommenderEvaluator evaluator = new MAEEvaluator();
+	recommender.evaluate(evaluator);
+
+	// recommendation results
+	List recommendedItemList = recommender.getRecommendedList();
+	RecommendedFilter filter = new GenericRecommendedFilter();
+    recommendedItemList = filter.filter(recommendedItemList);
+    
     }
 }
